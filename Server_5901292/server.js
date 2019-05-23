c98750657f9e5b34109de765aa403081f7ebb3cd
Sahapat <sahapat.starcaft@gmail.com>
2019-05-24 00:19:43 +0700

    Implement server for test connection

--- Server_5901292/server.js
+++ Server_5901292/server.js
@@ -2,36 +2,26 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-var playerName = [];
-var numPlayer = 0;
+var usernames = {};
 
 server.listen(3000, function () {
     console.log("Server runing at port 3000");
 })
 
 io.on('connection', function (socket) {
-    socket.emit('login require');
+    socket.emit('connect');
 
-    socket.on('login', function (data) {
-        if (numPlayer <= 1) {
-            playerName.push(data.name);
-            numPlayer += 1;
+    socket.on('login',function(data)
+    {
+        socket.username = data.name;
+        usernames[data.name] = data.name;
 
-            var resLogin =
-            {
-                name: data.name,
-                characterId: numPlayer-1
-            }
+        socket.emit('connected');
+    })
 
-            console.log("player: "+data.name+" has connected");
-            socket.broadcast.emit('otherPlayerConnected', resLogin);
-            socket.emit('connected', resLogin);
-
-            if(numPlayer >= 2)
-            {
-                socket.broadcast.emit('game start');
-                socket.emit('game start');
-            }
-        }
+    socket.on('disconnect',function()
+    {
+        delete usernames[socket.username];
+        io.sockets.emit('update user',socket.username);
     })
 })
