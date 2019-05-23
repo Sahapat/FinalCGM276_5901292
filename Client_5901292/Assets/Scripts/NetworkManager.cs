1f2155e4b177213e8c52888666f9d03f440b2b33
Sahapat <sahapat.starcaft@gmail.com>
2019-05-23 17:23:11 +0700

    Create log to check

--- Client_5901292/Assets/Scripts/NetworkManager.cs
+++ Client_5901292/Assets/Scripts/NetworkManager.cs
@@ -5,37 +5,35 @@ using SocketIO;
 
 public class NetworkManager : MonoBehaviour
 {
-    private SocketIOComponent m_socketIoComponent = null;
-
-    void Awake()
-    {
-        m_socketIoComponent = GetComponent<SocketIOComponent>();
-    }
+    [SerializeField] SocketIOComponent m_socketIoComponent = null;
 
     void Start()
     {
-        m_socketIoComponent.On("login require",OnLoginRequire);
-        m_socketIoComponent.On("connected",OnConnected);
-        m_socketIoComponent.On("otherPlayerConnected",OnOtherPlayerConnected);
-        m_socketIoComponent.On("game start",OnGameStart);
+        m_socketIoComponent.On("login require", OnLoginRequire);
+        m_socketIoComponent.On("connected", OnConnected);
+        m_socketIoComponent.On("otherPlayerConnected", OnOtherPlayerConnected);
+        m_socketIoComponent.On("game start", OnGameStart);
     }
     void OnConnected(SocketIOEvent socketIOEvent)
     {
         var data = socketIOEvent.data.ToString();
         GameCore.clientPlayerData = PlayerDataJson.CreateFromJson(data);
+        print("Connect success");
     }
     void OnOtherPlayerConnected(SocketIOEvent socketIOEvent)
     {
         var data = socketIOEvent.data.ToString();
         GameCore.otherPlayerData = PlayerDataJson.CreateFromJson(data);
+        print("player: "+GameCore.otherPlayerData.name+" has connected");
     }
     void OnGameStart(SocketIOEvent socketIOEvent)
     {
         GameCore.gamemanager.SetUp();
+        print("Game start");
     }
     void OnLoginRequire(SocketIOEvent socketIOEvent)
     {
-        string data = JsonUtility.ToJson(new PlayerDataJson(MainMenu.inputString,0));
-        m_socketIoComponent.Emit("login",new JSONObject(data));
+        string data = JsonUtility.ToJson(new PlayerDataJson(MainMenu.inputString, 0));
+        m_socketIoComponent.Emit("login", new JSONObject(data));
     }
 }
