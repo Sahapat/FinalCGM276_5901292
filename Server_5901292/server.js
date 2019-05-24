5014d5759d3a058e62cacf5705b9a1a0cab1dbe4
Sahapat <sahapat.starcaft@gmail.com>
2019-05-24 14:52:49 +0700

    Fix error not define lobby response

--- Server_5901292/server.js
+++ Server_5901292/server.js
@@ -3,9 +3,9 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 var defualtLobby = 'main';
-var lobbys = ['lobby1','lobby2','lobby3','lobby4','lobby5'];
-var lobbyHost = ['empty','empty','empty','empty','empty'];
-var playerInlobbys = [0,0,0,0,0];
+var lobbys = ['lobby1', 'lobby2', 'lobby3', 'lobby4', 'lobby5'];
+var lobbyHost = ['empty', 'empty', 'empty', 'empty', 'empty'];
+var playerInlobbys = [0, 0, 0, 0, 0];
 
 server.listen(3000, function () {
     console.log("Server runing at port 3000");
@@ -14,49 +14,46 @@ server.listen(3000, function () {
 io.on('connection', function (socket) {
     socket.emit('connect');
 
-    socket.on('login',function(data)
-    {
+    socket.on('login', function (data) {
         socket.username = data.name;
         socket.lobby = defualtLobby;
         socket.emit('connected');
         console.log(data.name + " has connected");
     })
-    socket.on('hosting',function()
-    {
+    socket.on('hosting', function () {
         var lobbyIndex = checkEmptyLobby();
-        if(lobbyIndex == -1)
-        {
+        if (lobbyIndex == -1) {
             socket.emit('not hostable');
             console.log("lobby is full");
         }
-        else
-        {
+        else {
             socket.leave(socket.lobby);
             socket.join(lobbys[lobbyIndex]);
             playerInlobbys[lobbyIndex] = 1;
             lobbyHost[lobbyIndex] = socket.username;
             socket.emit('hostable');
-            socket.broadcast.to(defualtLobby).emit('update lobby list',resLobbyList);
-            console.log(socket.username+" host at "+lobbys[lobbyIndex]);
+            var resLobbyList =
+            {
+                hostNames: lobbyHost,
+                lobbyCap: playerInlobbys
+            }
+            socket.broadcast.to(defualtLobby).emit('update lobby list', resLobbyList);
+            console.log(socket.username + " host at " + lobbys[lobbyIndex]);
         }
     })
-    socket.on('lobby list',function()
-    {
-        var resLobbyList = 
+    socket.on('lobby list', function () {
+        var resLobbyList =
         {
-            hostNames:lobbyHost,
-            lobbyCap:playerInlobbys
+            hostNames: lobbyHost,
+            lobbyCap: playerInlobbys
         }
-        socket.emit('update lobby list',resLobbyList);
+        socket.emit('update lobby list', resLobbyList);
         console.log("update lobby list");
     })
 })
-function checkEmptyLobby()
-{
-    for(var i =0;i<5;i++)
-    {
-        if(playerInlobbys[i] == 0)
-        {
+function checkEmptyLobby() {
+    for (var i = 0; i < 5; i++) {
+        if (playerInlobbys[i] == 0) {
             return i;
         }
     }
